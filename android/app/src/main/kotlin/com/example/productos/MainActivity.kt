62068ae923da6900beef6332fbb677ef4b055520
Reyacido <xerickxmichelex@gmail.com>
2025-04-07 17:42:48 -0600

    upgrade

--- android/app/src/main/kotlin/com/example/productos/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.productos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/productos_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.productos_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
